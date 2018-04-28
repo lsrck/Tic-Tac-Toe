ed0e1efa478215f99b5cb6c5c97b5330b51977a9
Louis Aurick <louisaurick@ntymail.com>
2018-04-28 11:38:49 +0200

    initialize

---
+++ tictactoe.rb
@@ -0,0 +1,23 @@
+class Board
+
+end
+
+class BoardCase
+
+end
+
+class Player
+
+end
+
+class Game
+
+end
+
+
+#------------------------------------#
+
+#Board's initialization
+game = Game.new
+
+#Players' initialization
